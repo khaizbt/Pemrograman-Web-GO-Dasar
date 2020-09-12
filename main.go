a89ddcfc41348aeda986ca375fa788b54c64c695
Khaiz Badaru Tammam <tammam.dev@gmail.com>
2020-09-12 23:50:35 +0700

    Hello World

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main()  {
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", handlerIndex)
+	http.HandleFunc("/hello", handlerHello)
+
+	var address = "localhost:9000"
+	fmt.Printf("Server started at %s \n", address)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func handlerIndex(w http.ResponseWriter, r *http.Request)  {
+	var message = "Welcome"
+
+	w.Write([]byte(message))
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request)  {
+	var message = "Hello World"
+	w.Write([]byte(message))
+}
